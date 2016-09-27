a9df4c80cfd66040119038202d61938f858c63c8
dbarrios <dbarrios@onpe.gob.pe>
2016-09-27 11:03:53 -0500

    Mejoras

---
+++ app/src/main/java/pe/com/danielbarrios/musicplayer/bean/Constantes.java
@@ -0,0 +1,12 @@
+package pe.com.danielbarrios.musicplayer.bean;
+
+/**
+ * Created by DBarrios on 27/09/2016.
+ */
+public class Constantes {
+
+    public static final String TAG_LOG = "MusicPlayer";
+    public static final String CONFIG_FILE_MUSIC = "tempData.txt";
+
+}
+
--- app/src/main/java/pe/com/danielbarrios/musicplayer/view/ListaMusicaActivity.java
+++ app/src/main/java/pe/com/danielbarrios/musicplayer/view/ListaMusicaActivity.java
@@ -1,50 +1,85 @@
 package pe.com.danielbarrios.musicplayer.view;
 
-import android.app.Activity;
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
+import android.media.MediaScannerConnection;
 import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.TextView;
-
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import pe.com.danielbarrios.musicplayer.R;
 import pe.com.danielbarrios.musicplayer.bean.CancionBean;
+import pe.com.danielbarrios.musicplayer.bean.Constantes;
 import pe.com.danielbarrios.musicplayer.util.AdapterView;
 
 public class ListaMusicaActivity extends AppCompatActivity {
 
-    final String MEDIA_PATH = Environment.getExternalStorageDirectory()
-            .getPath() + "/";
-    private ArrayList<HashMap<String, String>> songsList = new ArrayList();
+    final String MEDIA_PATH = Environment.getExternalStorageDirectory().getPath() + "/";
     private String mp3Pattern = ".mp3";
-    private TextView textViewListaMusica;
     ArrayList<CancionBean> arrayListaCanciones = new ArrayList();
-    String texto = "";
-    CancionBean[] listaCanciones = new CancionBean[20];
-    int indice = 0;
     MediaPlayer mediaPlayer;
+    File traceFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_musica);
         inicializarVariables();
+        validarDataInicial();
     }
+
+    private void validarDataInicial() {
+
+        if (traceFile.exists()){
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(traceFile));
+                String jsonData = reader.readLine();
+                arrayListaCanciones = new Gson().fromJson(jsonData, new TypeToken<ArrayList<CancionBean>>() {}.getType());
+                setearAdapters();
+            } catch (Exception e) {
+                Log.e(Constantes.TAG_LOG, "Error en el grabado de archivo conf. Error: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void crearDatosStorage() {
+
+        try
+        {
+            if (!traceFile.exists())
+                traceFile.createNewFile();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(traceFile, false /*append*/));
+            writer.write(new Gson().toJson(arrayListaCanciones));
+            writer.close();
+            MediaScannerConnection.scanFile((Context) (this),new String[]{traceFile.toString()},null,null);
+
+        }
+        catch (Exception e){
+            Log.e(Constantes.TAG_LOG,"Error en el grabado de archivo conf. Error: "+e.getMessage());
+        }
+    }
+
 
     private void inicializarVariables() {
 
+        traceFile = new File(((Context)this).getExternalFilesDir(null),Constantes.CONFIG_FILE_MUSIC);
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
@@ -60,7 +95,6 @@ public class ListaMusicaActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_update_item:
-                System.out.println("Aprete el boton");
                 arrayListaCanciones.clear();
                 updateMusicList();
                 return true;
@@ -71,21 +105,10 @@ public class ListaMusicaActivity extends AppCompatActivity {
 
     private void updateMusicList() {
         getPlayList();
-
-        for (HashMap<String,String> musica : songsList){
-
-            texto = texto + "\n" +"Titulo: "+musica.get("songTitle")+ " ; Path: "+musica.get("songPath");
-            System.out.println(texto);
-            //System.out.println("Titulo: " + musica.get("songTitle"));
-            //System.out.println("Path: " + musica.get("songPath"));
-
-
-        }
-
-        inicializarAdapters();
+        setearAdapters();
     }
 
-    public ArrayList<HashMap<String, String>> getPlayList() {
+    public void getPlayList() {
         System.out.println(MEDIA_PATH);
         if (MEDIA_PATH != null) {
             File home = new File(MEDIA_PATH);
@@ -101,8 +124,7 @@ public class ListaMusicaActivity extends AppCompatActivity {
                 }
             }
         }
-        // return songs list array
-        return songsList;
+        crearDatosStorage();
     }
 
     private void scanDirectory(File directory) {
@@ -123,23 +145,15 @@ public class ListaMusicaActivity extends AppCompatActivity {
 
     private void addSongToList(File song) {
         if (song.getName().endsWith(mp3Pattern)) {
-            HashMap<String, String> songMap = new HashMap<String, String>();
-            songMap.put("songTitle",song.getName().substring(0, (song.getName().length() - 4)));
-            songMap.put("songPath", song.getPath());
-
             CancionBean cancionBean = new CancionBean();
             cancionBean.setNombreCancion(song.getName().substring(0, (song.getName().length() - 4)));
             cancionBean.setRutaCancion(song.getPath());
-            //listaCanciones[indice] = cancionBean;
             arrayListaCanciones.add(cancionBean);
-            System.out.println("ITEM : " +cancionBean.getNombreCancion());
-            // Adding each song to SongList
-            songsList.add(songMap);
-            indice++;
+
         }
     }
 
-    public void inicializarAdapters(){
+    public void setearAdapters(){
         AdapterView adapter = new AdapterView(this, R.layout.list_item_song,arrayListaCanciones.toArray());
         ListView listViewItems = (ListView)findViewById(R.id.list_view_lista_canciones);
         listViewItems.setAdapter(adapter);
@@ -149,8 +163,6 @@ public class ListaMusicaActivity extends AppCompatActivity {
     android.widget.AdapterView.OnItemClickListener mOnClickListener = new android.widget.AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(android.widget.AdapterView<?> parent, View view, int position, long id) {
-            //play musica
-
             try {
 
                 //https://www.tutorialspoint.com/android/android_mediaplayer.htm
